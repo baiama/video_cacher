bf753ad616cb493b999369768a1ff5eb29894b20
baiama <primkulovbaiaman@gmail.com>
2021-11-01 18:38:36 +0600

    dsa

--- lib/player.dart
+++ lib/player.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class VideoListView extends StatelessWidget {
+class VideoListView extends StatefulWidget {
   const VideoListView({Key? key}) : super(key: key);
+
+  @override
+  State<VideoListView> createState() => _VideoListViewState();
+}
+
+class _VideoListViewState extends State<VideoListView> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://www.dropbox.com/s/oc03mz30413sfel/clouds.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play();
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
     );
   }
 }
